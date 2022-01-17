5ec15d2920a32034ae888a5e55fbaa45f0edc6c4
Ömer Faruk ARTAN <o.farukartan@hotmail.com>
2022-01-18 00:04:04 +0300

    Initial check-in of module Contacts

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Entities.Concrete;
+using System.Linq.Expressions;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+        }
+    }
+}
---
+++ DataAccess/Concrete/ContactContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete
+{
+    public class ContactContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Database=PhoneBook;Username=omerartan;Password=postgres"
+                );
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
